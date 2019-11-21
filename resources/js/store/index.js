408c74ed3d3dacad6d1fd58186caf3a6e7d696ce
horitomo <ths75149@gmail.com>
2019-11-21 17:38:07 +0900

    fileを読み込み

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from './auth'
+import file from './file'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
 	modules: {
-		auth
+		auth,
+		file
 	}
 })
 
