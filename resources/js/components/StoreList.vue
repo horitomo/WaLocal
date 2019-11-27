c1071466983a07003f3197c7e6fa7fc4319403b4
horitomo <ths75149@gmail.com>
2019-11-27 19:36:28 +0900

    店名表示コンポーネント

---
+++ resources/js/components/StoreList.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="panel">
+		<form @submit.prevent="go">
+			<p>店名：{{ this.item.storeName }}</p>
+			<p>住所：{{ this.item.address }}</p>
+			<button>選択</button>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  methods:{
+	  async go(){
+		  console.log(this.item.storeName)
+		  console.log(this.$store.getters['file/departurePosition'])
+		  console.log(this.$store.getters['file/arrivalPosition'])
+	  }
+  }
+}
+</script>
