dbd7d703707e4a56933f2591d24a331ee4cfc68b
horitomo <ths75149@gmail.com>
2019-11-27 19:34:49 +0900

    保存データ

--- resources/js/store/file.js
+++ resources/js/store/file.js
@@ -1,6 +1,14 @@
 const state = {
 	arrival : null,
-	departure : null
+	departure : null,
+	departurePosition:{
+		lat: null,
+		lng:null
+	},
+	arrivalPosition:{
+		lat: null,
+		lng: null
+	}
 }
 
 const getters = {
@@ -10,13 +18,24 @@ const getters = {
 	checkA: state => !! state.arrival,
 	checkD: state => !! state.departure,
 	arrival: state => state.arrival ? state.arrival : '',
-	departure: state => state.departure ? state.departure : ''
+	departure: state => state.departure ? state.departure : '',
+	arrivalPosition: state => state.arrivalPosition ? state.arrivalPosition : '',
+	departurePosition: state => state.departurePosition ? state.departurePosition : ''
+
 }
 
 const mutations = {
 	setPlace (state, data) {
 		state.arrival = data.arrival
 		state.departure = data.departure
+	},
+	setDPosition (state, data) {
+		state.departurePosition.lat = data.lat
+		state.departurePosition.lng = data.lng
+	},
+	setAPosition (state, data) {
+		state.arrivalPosition.lat = data.lat
+		state.arrivalPosition.lng = data.lng
 	}
 }
 
@@ -24,6 +43,14 @@ const actions = {
 	async register (context, data) {
 		// const response = await axios.post('/api/register', data)
 		context.commit('setPlace', data)
+	},
+	registerDPosition (context, data) {
+		// const response = await axios.post('/api/register', data)
+		context.commit('setDPosition', data)
+	},
+	async registerAPosition (context, data) {
+		// const response = await axios.post('/api/register', data)
+		context.commit('setAPosition', data)
 	}
 }
 
