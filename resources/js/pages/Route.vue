358d9ba5732fd1fe2e88b49aed14f796ebf9af3e
horitomo <ths75149@gmail.com>
2019-12-03 11:06:06 +0900

    mapコンポーネントを表示するページ

---
+++ resources/js/pages/Route.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="panel">
+		<Googlemap />
+		<form @submit.prevent="prev">
+			<button>戻る</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	import Googlemap from '../components/Googlemap'
+	export default {
+		methods: {
+			prev(){
+				this.$router.push('/list')
+			}
+		},
+		components:{
+			Googlemap
+		}
+	}
+</script>
