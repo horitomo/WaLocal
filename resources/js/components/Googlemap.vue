8b43da8a6537b3761b00bc2ea7a9917be012b689
horitomo <ths75149@gmail.com>
2019-12-03 11:03:11 +0900

    入力したaddressからルートを作成して表示

--- resources/js/components/Googlemap.vue
+++ resources/js/components/Googlemap.vue
@@ -8,7 +8,7 @@ export default {
   data() {
     return {
       map: null,
-      mapPosition: {lat: 35.658034, lng: 139.701636}
+      mapPosition: {lat: this.$store.getters['file/lat'], lng: this.$store.getters['file/lng']}
     };
   },
   methods: {
@@ -21,9 +21,12 @@ export default {
       ref.parentNode.insertBefore(script, ref);
     },
     initMap() {
+      var wayPoints = new Array();
+      var waypoint = this.$store.getters['file/address'];
+      wayPoints.push({location: waypoint})
       this.map = new google.maps.Map(document.getElementById('map'), {
         center: this.mapPosition,
-        zoom: 11
+        zoom: 14
       });
 
         var d = new google.maps.DirectionsService(); // ルート検索オブジェクト
@@ -31,14 +34,14 @@ export default {
         map: this.map, // 描画先の地図
         preserveViewport: true, // 描画後に中心点をずらさない
         });
-
+        
         var request = {
         //origin: new google.maps.LatLng(35.681382,139.766084), // 出発地
         //origin: this.data.routeForm.arrival,
         origin : this.$store.getters['file/arrival'],
         //destination: this.mapPosition, // 目的地
         destination: this.$store.getters['file/departure'],
-//        waypoints:wayPoints,
+        waypoints:wayPoints,
         travelMode: google.maps.DirectionsTravelMode.WALKING, // 交通手段(歩行。DRIVINGの場合は車)
         };
         // ルート検索
