92c8da4943b7be7752358d35161cca648094104e
horitomo <ths75149@gmail.com>
2019-12-04 12:53:48 +0900

    first commit

---
+++ database/migrations/2019_12_03_051308_create_route_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRouteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('route', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('departureAddress');
+            $table->string('arrivalAddress');
+            $table->string('wayPoint');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('route');
+    }
+}
