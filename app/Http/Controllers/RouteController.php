3c9e740a64c97e10218ef0b6c1f4f852c8dffd0d
horitomo <ths75149@gmail.com>
2019-12-04 12:55:00 +0900

    createを変更db(route)にinsert

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Route;
 
 class RouteController extends Controller
 {
     //
     public function create(Request $request)
     {
-        $route = new Route;
-        $route->departureAddress = $request->departure;
-        $route->arrivalAddress = $request->arrival;
-        $route->wapPoint = $request->wayPoint;
-        $route->save();
-        return redirect('/');
+        $departureAddress = $request->input("departureAddress");
+        $arrivalAddress = $request->input("arrivalAddress");
+        $wayPoint = $request->input("wayPoint");
+        DB::table('route')->insert(
+            ['departureAddress' => $departureAddress, 'arrivalAddress' => $arrivalAddress, 'wayPoint' => $wayPoint]
+        );
     }
 }
