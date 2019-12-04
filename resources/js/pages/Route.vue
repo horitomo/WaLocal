e45df1d10db0d2bf2b02491bf4f08c6cf4bdf77e
horitomo <ths75149@gmail.com>
2019-12-04 12:53:12 +0900

    ルート登録メソッドの修正

--- resources/js/pages/Route.vue
+++ resources/js/pages/Route.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="panel">
 		<Googlemap2 />
+		<form @submit.prevent="route">
+			<button>登録</button>
+		</form>
 		<form @submit.prevent="prev">
 			<button>戻る</button>
 		</form>
@@ -10,7 +13,21 @@
 <script>
 	import Googlemap2 from '../components/Googlemap2'
 	export default {
+		data() {
+			return {
+				routeRegisterForm : {
+					departureAddress : this.$store.getters['file/departure'],
+					arrivalAddress : this.$store.getters['file/arrival'],
+					wayPoint : this.$store.getters['file/address']
+				}
+			};
+		},
 		methods: {
+			async route(){
+				const responce = await axios.post('/api/route', this.routeRegisterForm)
+				// トップページに移動する
+				this.$router.push('/')
+			},
 			prev(){
 				this.$router.push('/list')
 			}
