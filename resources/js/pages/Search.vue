b198feada115a4a55418a1ae70c7900a2fe27196
horitomo <ths75149@gmail.com>
2019-12-10 11:10:21 +0900

    first commit

---
+++ resources/js/pages/Search.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<SearchWindow />
+	</div>
+</template>
+<script>
+import SearchWindow from '../components/SearchWindow'
+export default {
+	data(){
+		return{
+			routeForm: {
+				departure : '',
+				arrival : ''
+			}
+		}
+	},
+	methods :{
+		route(routeForm){
+			this.routeForm = routeForm
+			console.log(this.routeForm)
+		}
+	},
+	components:{
+		SearchWindow
+	}
+}
+</script>
