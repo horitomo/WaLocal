41f137068e476ecef9c01a97c7a255cb808846a2
horitomo <ths75149@gmail.com>
2019-12-10 11:06:04 +0900

    ルート登録のcreateと一覧取得のindex追記

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -15,8 +15,19 @@ class RouteController extends Controller
         $departureAddress = $request->input("departureAddress");
         $arrivalAddress = $request->input("arrivalAddress");
         $wayPoint = $request->input("wayPoint");
+        $lat = $request->input("lat");
+        $lng = $request->input("lng");
         DB::table('route')->insert(
-            ['departureAddress' => $departureAddress, 'arrivalAddress' => $arrivalAddress, 'wayPoint' => $wayPoint]
+            ['departureAddress' => $departureAddress, 'arrivalAddress' => $arrivalAddress, 'wayPoint' => $wayPoint, 'lat' => $lat, 'lng' => $lng]
         );
+    }
+
+    public function index(Request $request)
+    {
+        $departureAddress = $request->input("departureAddress");
+        $arrivalAddress = $request->input("arrivalAddress");
+        $routes = DB::select('select * from route where departureAddress = ?',["東京都八王子市上壱分方町３５３−１１５"]);
+        //$routes = DB::table('route')->get();
+        return $routes;
     }
 }
