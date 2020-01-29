4979b2df3c4f104d3f83aea22e2a9ca1ae7404b9
horitomo <ths75149@gmail.com>
2020-01-29 10:52:01 +0900

    ルート結果表記画面

---
+++ resources/js/pages/ResultRoute.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<Googlemap2 />
+		<RouterLink to="/listStore">
+			戻る
+		</RouterLink>
+		<RouterLink to="/">
+			TOP
+		</RouterLink>
+	</div>
+</template>
+<script>
+import Googlemap2 from '../components/Googlemap2'
+export default {
+	components:{
+		Googlemap2
+	}
+}
+</script>
