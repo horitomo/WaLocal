85d5a4908c02de887067652c3a578257d6f33c48
horitomo <ths75149@gmail.com>
2020-01-29 10:44:58 +0900

    storeテーブル作成

---
+++ database/migrations/2019_12_17_045755_create_store_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('storeName');
+            $table->string('storeAddress');
+            $table->char('storeCategory',2);
+            $table->double('lat',20,14);
+            $table->double('lng',20,14);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store');
+    }
+}
