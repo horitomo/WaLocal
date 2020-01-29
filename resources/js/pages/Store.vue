92e96b2fe67edadeb0e1a3403ad217aa79ada8d8
horitomo <ths75149@gmail.com>
2020-01-29 10:50:01 +0900

    store登録

---
+++ resources/js/pages/Store.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="panel">
+		<StoreRegister  />
+	</div>
+</template>
+
+<script>
+import StoreRegister from '../components/StoreRegister'
+export default {
+	components:{
+		StoreRegister
+	}
+}
+</script>
