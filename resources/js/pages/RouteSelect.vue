d4ef7288615ea4d85b33c9e5b1c8b56d543a172e
horitomo <ths75149@gmail.com>
2020-01-29 10:50:42 +0900

    データベースからお店の情報を取得するように変更

--- resources/js/pages/RouteSelect.vue
+++ resources/js/pages/RouteSelect.vue
@@ -2,6 +2,11 @@
   <div>
     <p>出発地：{{ this.departureS }}</p>
     <p>到着地：{{ this.arrivalS }}</p>
+    <form @submit.prevent="show">
+      <button type="submit" >
+      表示
+    </button>
+    </form>
     <StoreList
         v-for="place in places"
         v-bind:key="place.id"
@@ -20,24 +25,23 @@ export default {
       mapPosition: {lat: 35.658034, lng: 139.701636},
       departureS : this.$store.getters['file/departure'],
       arrivalS : this.$store.getters['file/arrival'],
-      departurePosition:{
-	      lat:'',
-	      lng:''
-      },
-      arrivalPosition: {
-	      lat:'',
-	      lng:''
+      position : {
+        departurelat:'',
+        departurelng:'',
+        arrivallat:'',
+        arrivallng:''        
       },
       places: []
     };
   },
   methods: {
-    isList(){
+    async isList(){
       this.loadJS(`https://maps.googleapis.com/maps/api/js?key=${GOOGLEMAP_API_KEY}&callback=initMap`)
       let self = this
       window.initMap = self.initMap
-      this.places[0] = {"storeName":"一陽来福","address":"東京都八王子市楢原町472-1","lat":35.6806528,"lng":139.30740349999996}
-      this.places[1] = {"storeName":"みんみんラーメン　本店","address":"東京都八王子市楢原町437-1","lat":35.6806528,"lng":139.30740349999996}
+      // this.places[0] = {"storeName":"一陽来福","address":"東京都八王子市楢原町472-1","lat":35.6806528,"lng":139.30740349999996}
+      // this.places[1] = {"storeName":"みんみんラーメン　本店","address":"東京都八王子市楢原町437-1","lat":35.6806528,"lng":139.30740349999996}
+      // this.places[2] = {"storeName" : "大進亭","address":"東京都八王子市大楽寺町184","lat":35.6780926,"lng":139.2836479}
     },
     loadJS(src) {
       const ref = window.document.getElementsByTagName("script")[0];
@@ -58,9 +62,7 @@ export default {
       geocoderFrom.geocode({ address: from }, function(results, status){
       if (status === 'OK' && results[0]){  //status を確認して処理開始
         depP.lat = results[0].geometry.location.lat();
-        self.departurePosition.lat = results[0].geometry.location.lat();
         depP.lng = results[0].geometry.location.lng();
-        self.departurePosition.lng = results[0].geometry.location.lng();
       }
       });
       
@@ -72,7 +74,18 @@ export default {
       });
       setTimeout(() => this.$store.dispatch('file/registerDPosition', depP), 500);
       setTimeout(() => this.$store.dispatch('file/registerAPosition', arriP), 500);
-	  },
+      setTimeout(() => this.position.departurelat = depP.lat, 700);
+      setTimeout(() => this.position.arrivallat = arriP.lat, 700);
+      setTimeout(() => this.position.departurelng = depP.lng, 700);
+      setTimeout(() => this.position.arrivallng = arriP.lng, 700);
+    },
+    async show() {
+      console.log(this.position)
+      const response = await axios.post('/api/store/list', this.position)
+      console.log(response)
+      let self = this
+			self.places = response.data
+    }
   },
   mounted : function(){
     //this.$store.dispatch('file/registerDPosition', this.departurePosition)
