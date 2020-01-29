c5c6c8736bf749019cddb5230719eaa01f655bed
horitomo <ths75149@gmail.com>
2020-01-29 10:51:21 +0900

    first commit

--- resources/js/pages/Route.vue
+++ resources/js/pages/Route.vue
@@ -18,7 +18,9 @@
 				routeRegisterForm : {
 					departureAddress : this.$store.getters['file/departure'],
 					arrivalAddress : this.$store.getters['file/arrival'],
-					wayPoint : this.$store.getters['file/address']
+					wayPoint : this.$store.getters['file/address'],
+					lat : this.$store.getters['file/lat'], 
+					lng: this.$store.getters['file/lng']
 				}
 			};
 		},
@@ -26,7 +28,7 @@
 			async route(){
 				const responce = await axios.post('/api/route', this.routeRegisterForm)
 				// トップページに移動する
-				this.$router.push('/')
+				this.$router.push('/search')
 			},
 			prev(){
 				this.$router.push('/list')
