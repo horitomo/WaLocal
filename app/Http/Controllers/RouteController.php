49497f1708b09e1fccf5759f3a4b12a9dce967b1
horitomo <ths75149@gmail.com>
2020-01-29 10:43:39 +0900

    exampleのクラスを追記

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -36,7 +36,7 @@ class RouteController extends Controller
     {
         $departureB = $request->input("departure");
         $arrivalB = $request->input("arrival");
-        $routes = DB::select('select * from route where departureAddress = ?',[$departureB]);
+        $routes = DB::select('select * from route where departureAddress = ? AND arrivalAddress = ?',[$departureB,$arrivalB]);
         //$routes = DB::table('route')->get();
         return $routes;
     }
