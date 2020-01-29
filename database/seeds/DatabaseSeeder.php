70a897f41727ec6547e82d22daf5f198cc984279
horitomo <ths75149@gmail.com>
2020-01-29 10:45:20 +0900

    仮データ

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'horitomo',
+            'email' => 'ths75149@gmail.com',
+            'password' => bcrypt('okttm0425'),
+        ]);
     }
 }
