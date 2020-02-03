c5cabbf5322d385b6482ad58d5ffcd5ec32b8077
horitomo <ths75149@gmail.com>
2020-02-03 16:36:39 +0900

    店名とジャンルを表記

--- resources/js/components/RouteList.vue
+++ resources/js/components/RouteList.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="panel">
 		<form @submit.prevent="mapGo">
+			<p>店名：{{ this.item.storeName }}</p>
 			<p>住所：{{ this.item.wayPoint }}</p>
+			<p>ジャンル：{{ this.docdoc }}</p>
 			<button>選択</button>
 		</form>
 	</div>
@@ -14,11 +16,36 @@ export default {
 			required: true
 		}
 	},
+	data(){
+		return {
+			docdoc : ''
+		}
+
+	},
 	methods:{
 		async mapGo(){
 			this.item.storeAddress = this.item.wayPoint
 			await this.$store.dispatch('file/storeLatLng',this.item);
 			this.$router.push('/result')
+		},
+		category($category){
+			if($category == "01"){
+				this.docdoc = "麺類"
+			}else if($category == "02"){
+				this.docdoc = "和食"
+			}else if($category == "03"){
+				this.docdoc = "洋食"
+			}else if($category == "04"){
+				this.docdoc = "中華"
+			}
+		}
+	},
+	watch: {
+		$route: {
+			async handler () {
+				await this.category(this.item.storeCategory)
+			},
+			immediate: true
 		}
 	}
 }
